7b092a8d109b6881a2b83e7841a39632c01626f9
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2022-08-28 15:50:21 +0200

    Create scp3008.lua
    OMG

---
+++ scp3008.lua
@@ -0,0 +1,154 @@
+--Credits to the library maker
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
+local Flags = Library.Flags
+--Library.DefaultColor = Color3.fromRGB(65, 143, 232)
+
+local Window = Library:Window({
+   Text = "Exploits"
+})
+
+local Window2 = Library:Window({
+   Text = "ESP"
+})
+
+local Window3 = Library:Window({
+   Text = "Fun"
+})
+
+local Window4 = Library:Window({
+   Text = "Character"
+})
+
+local Window5 = Library:Window({
+   Text = "Settings"
+})
+Library:Notification({
+           Text = "Press ESC to access the gui",
+           Duration = 10
+})
+
+Window:Button({
+   Text = "God Mode",
+   Callback = function()
+game.Players.LocalPlayer.Character.Stat:Destroy()
+game.Players.LocalPlayer.Character.Health:Destroy()
+game.Players.LocalPlayer.Character.FallDamage:Destroy()
+   end
+})
+Window:Button({
+	Text = "Daytime",
+	Callback = function()
+		game:GetService("Lighting").Brightness = 1
+	game:GetService("Lighting").ClockTime = 12
+	game:GetService("Lighting").FogEnd = 300
+	game:GetService("Lighting").GlobalShadows = true
+	game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(0.47, 0.47, 0.47)
+	Library:Notification({
+           Text = "Click on Daytime when it turns night",
+           Duration = 10
+})
+end
+})
+
+
+Window2:Button({
+   Text = "Player ESP",
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-esp/main/esp.lua"))()
+end
+})
+Window2:Button({
+   Text = "Employee ESP",
+   Callback = function()
+    for __,v in pairs(game:GetService("Workspace").GameObjects.Physical.Employees:GetChildren()) do
+      if v:IsA("Model") then
+        local a = Instance.new("BillboardGui",v)
+        a.Name = "Lol"
+        a.Size = UDim2.new(7,0,7,0)
+        a.AlwaysOnTop = true
+        a.MaxDistance = 250
+        local b = Instance.new("Frame",a)
+        b.Size = UDim2.new(1,0, 1,0)
+        b.BackgroundTransparency = 1
+        b.BorderSizePixel = 0
+        b.BackgroundColor3 = Color3.new(0, 0, 0)
+        local c = Instance.new('TextLabel',b)
+		c.TextColor3 = Color3.new(1, 0, 0)
+        c.Size = UDim2.new(2,0,2,0)
+        c.BorderSizePixel = 0
+        c.TextSize = 15
+        c.Text = v.Name
+        c.BackgroundTransparency = 1
+      end
+  end
+end
+})
+
+Window3:Button({
+	Text = "Start Infinite Yield",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/HPHub/main/IYNotByMe.lua"))()
+	end
+})
+
+Window4:Slider({
+   Text = "WalkSpeed",
+   Default = 16,
+   Minimum = 16,
+   Maximum = 200,
+   Callback = function(value)
+       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+   end
+})
+Window4:Slider({
+   Text = "JumpPower",
+   Default = 50,
+   Minimum = 50,
+   Maximum = 250,
+   Callback = function(value)
+       game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+   end
+})
+Window4:Slider({
+   Text = "Gravity",
+   Default = 196,
+   Minimum = 0,
+   Maximum = 196,
+   Callback = function(value)
+       game.Workspace.Gravity = value
+   end
+})
+Window4:Slider({
+	Text = "HipHeight",
+	Default = 2,
+	Minimum = 2,
+	Maximum = 50,
+	Callback = function(value)
+		game.Players.LocalPlayer.Character.Humanoid.HipHeight = value
+	end
+})
+
+Window5:Label({
+   Text = "Made by: TRHP .gg/G3KVvt988s",
+   Color = Color3.fromRGB(214, 214, 214)
+})
+
+Window5:Keybind({
+   Text = "Toggle Library",
+   Default = Enum.KeyCode.LeftAlt,
+   Callback = function()
+       Library:Toggle()
+   end
+})
+
+wait(2)
+Toggle:Set({
+   Bool = true
+})
+
+while true do
+   if Flags.TestFlag then
+       warn("Toggled")
+   end
+   wait(1)
+end
