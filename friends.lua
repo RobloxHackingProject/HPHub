29157ea0dd7da0b51ec3f4c9639b70042d218fe0
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2022-10-30 10:38:55 +0100

    Update friends.lua

--- friends.lua
+++ friends.lua
@@ -774,9 +774,16 @@ Window5:Slider({
 	end
 })
 
-Window6:Label({
+Window6:Button({
    Text = "Made by: TRHP .gg/G3KVvt988s",
-   Color = Color3.fromRGB(214, 214, 214)
+   Color = Color3.fromRGB(214, 214, 214),
+   Callback = function()
+   setclipboard("discord.gg/G3KVvt988s")
+   	Library:Notification({
+           Text = "Copied to clipboad",
+           Duration = 3
+})
+   end
 })
 
 Window6:Keybind({
