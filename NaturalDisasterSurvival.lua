98f7bc5c67384f4909e927e893c83b7100331e7a
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2022-10-30 10:37:50 +0100

    Update NaturalDisasterSurvival.lua

--- NaturalDisasterSurvival.lua
+++ NaturalDisasterSurvival.lua
@@ -245,9 +245,16 @@ Window5:Button({
 	end
 })
 
-Window6:Label({
-	Text = "Made by: TRHP .gg/G3KVvt988s",
-	Color = Color3.fromRGB(214, 214, 214)
+Window6:Button({
+   Text = "Made by: TRHP .gg/G3KVvt988s",
+   Color = Color3.fromRGB(214, 214, 214),
+   Callback = function()
+   setclipboard("discord.gg/G3KVvt988s")
+   	Library:Notification({
+           Text = "Copied to clipboad",
+           Duration = 3
+})
+   end
 })
 
 Window6:Keybind({
