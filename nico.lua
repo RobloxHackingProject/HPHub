7b0ecd7bcafb55216ad52f290b906b6d5b12b092
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2022-12-30 17:54:03 +0100

    Update nico.lua

--- nico.lua
+++ nico.lua
@@ -149,8 +149,8 @@ Window4:Slider({
 })
 Window4:Slider({
 	Text = "HipHeight",
-	Default = 2.8,
-	Minimum = 2.8,
+	Default = game.Players.LocalPlayer.Character.Humanoid.HipHeight,
+	Minimum = 0,
 	Maximum = 50,
 	Callback = function(value)
 		game.Players.LocalPlayer.Character.Humanoid.HipHeight = value
