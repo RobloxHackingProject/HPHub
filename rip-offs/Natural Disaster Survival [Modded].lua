708fdea29709d6098fdc88918dbd1f76a4f7a98d
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2023-01-01 15:52:08 +0100

    Create Natural Disaster Survival [Modded].lua

---
+++ rip-offs/Natural Disaster Survival [Modded].lua
@@ -0,0 +1,259 @@
+--Credits to the library maker
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
+local Flags = Library.Flags
+--Library.DefaultColor = Color3.fromRGB(65, 143, 232)
+
+local Window = Library:Window({
+	Text = "Exploits"
+})
+
+local Window2 = Library:Window({
+	Text = "Teleports"
+})
+
+local Window3 = Library:Window({
+	Text = "Fun"
+})
+
+local Window4 = Library:Window({
+	Text = "Character"
+})
+local Window5 = Library:Window({
+	Text = "miscellaneous"
+})
+
+local Window6 = Library:Window({
+	Text = "Settings"
+})
+
+Window:Toggle({
+	Text = "Walk on Water",
+	Callback = function(bool)
+		if bool == false then do game.Workspace.WaterLevel.CanCollide = false
+				game.Workspace.WaterLevel.Size = Vector3.new(10, 1, 10)
+			end
+		end
+		if bool == true then do game.Workspace.WaterLevel.CanCollide = true
+				game.Workspace.WaterLevel.Size = Vector3.new(1000, 1, 1000)
+			end
+		end
+	end
+})
+
+Window:Toggle({
+	Text = "Choose Map",
+	Callback = function(bool)
+		if bool == false then do game.Players.LocalPlayer.PlayerGui.MainGui.MapVotePage.Visible = false
+			end
+		end
+		if bool == true then do game.Players.LocalPlayer.PlayerGui.MainGui.MapVotePage.Visible = true
+			end
+		end
+	end
+})
+Window:Toggle({
+	Text = "Solid Island",
+	Callback = function(bool)
+		for i, v in pairs (game.workspace:GetDescendants())do
+			if v.Name == 'LowerRocks' then -- change part to the name you want to look for
+				v.CanCollide = bool
+			end
+		end
+	end
+})
+Window:Button({
+	Text = "Launch Launch Land Rocket",
+	Callback = function()
+fireclickdetector(game:GetService("Workspace").Structure["Launch Land"]["SPACESHIP!!"].Shuttle.IgnitionButton.ClickDetector)
+fireclickdetector(game:GetService("Workspace").Structure["Launch Land"].RocketStand.ConsoleLower.ReleaseButtonLower.ClickDetector)
+fireclickdetector(game:GetService("Workspace").Structure["Launch Land"].RocketStand.ConsoleUpper.ReleaseButtonUpper.ClickDetector)
+fireclickdetector(game:GetService("Workspace").Structure["Launch Land"].LoadingTower.Console.ReleaseEntryBridge.ClickDetector)
+end
+})
+
+
+Window2:Button({
+	Text = "Island",
+	Callback = function(bool)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-108, 49, 0)
+	end
+})
+Window2:Button({
+	Text = "Tower",
+	Callback = function(bool)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-264, 196, 288)
+	end
+})
+
+Window3:Dropdown({
+	Text = "Water Color",
+	List = {"Black", "White", "Purple", "Cyan", "Reset"},
+	Callback = function(bool)
+		if bool == "Black" then do
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("Black")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("Black")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("Black")
+				end
+			end
+		end
+		if bool == "White" then do
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("White")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("White")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("White")
+				end
+			end
+		end
+		if bool == "Purple" then do
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("Bright violet")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("Bright violet")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("Bright violet")
+				end
+			end
+		end
+		if bool == "Cyan" then do
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("Cyan")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("Cyan")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("Cyan")
+				end
+			end
+		end
+		if bool == "Reset" then do
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("Bright blue")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("Bright blue")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("Bright blue")
+				end
+			end
+		end
+		end
+})
+Window3:Toggle({
+	Text = "Dark Mode",
+	Callback = function(bool)
+		if bool == false then do
+				game:GetService("Lighting").ClockTime = "14"
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("Bright blue")
+				game.Workspace.Structure.Cloud.BrickColor = BrickColor.new("Sand blue")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("Bright blue")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("Bright blue")
+				end
+			end
+		end
+		if bool == true then do
+				game:GetService("Lighting").ClockTime = "4"
+				game.Workspace.WaterLevel.BrickColor = BrickColor.new("Black")
+				game.Workspace.Structure.Cloud.BrickColor = BrickColor.new("Black")
+				game.Workspace.Structure.FloodLevel.BrickColor = BrickColor.new("Black")
+				local Children = game.Workspace.TsunamiWave:GetChildren()
+				for a = 1, #Children do
+					Children[a].BrickColor = BrickColor.new("Black")
+				end
+			end
+		end
+		end
+})
+Window3:Button({
+	Text = "Remove Ads",
+	Callback = function()
+game:GetService("Workspace").ShopSign:Destroy()
+game:GetService("Workspace").Rack:Destroy()
+end
+})
+Window3:Button({
+	Text = "Start Infinite Yield",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/HPHub/main/IYNotByMe.lua"))()
+	end
+})
+
+
+Window4:Slider({
+	Text = "JumpPower",
+	Default = 50,
+	Minimum = 50,
+	Maximum = 250,
+	Callback = function(value)
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+	end
+})
+Window4:Slider({
+	Text = "Gravity",
+	Default = 196,
+	Minimum = 0,
+	Maximum = 196,
+	Callback = function(value)
+		game.Workspace.Gravity = value
+	end
+})
+Window4:Slider({
+	Text = "HipHeight",
+	Default = game.Players.LocalPlayer.Character.Humanoid.HipHeight,
+	Minimum = 0,
+	Maximum = 50,
+	Callback = function(value)
+		game.Players.LocalPlayer.Character.Humanoid.HipHeight = value
+	end
+})
+
+Window5:Button({
+	Text = "Remove Sandstorm GUI",
+	Callback = function()
+		game.Players.LocalPlayer.PlayerGui.SandStormGui:destroy()
+	end
+})
+Window5:Button({
+	Text = "Remove Blizzard GUI",
+	Callback = function()
+		game.Players.LocalPlayer.PlayerGui.BlizzardGui:destroy()
+	end
+})
+Window5:Button({
+	Text = "Remove Fog",
+	Callback = function()
+		game:GetService("Lighting").FogEnd = 5000
+		game:GetService("Lighting").FogStart = 500
+	end
+})
+
+Window6:Button({
+   Text = "Made by: TRHP .gg/G3KVvt988s",
+   Color = Color3.fromRGB(214, 214, 214),
+   Callback = function()
+   setclipboard("discord.gg/G3KVvt988s")
+   	Library:Notification({
+           Text = "Copied to clipboad",
+           Duration = 3
+})
+   end
+})
+
+Window6:Keybind({
+	Text = "Toggle Library",
+	Default = Enum.KeyCode.LeftAlt,
+	Callback = function()
+		Library:Toggle()
+	end
+})
+
+wait(2)
+Toggle:Set({
+	Bool = true
+})
+
+while true do
+	if Flags.TestFlag then
+		warn("Toggled")
+	end
+	wait(1)
+end
