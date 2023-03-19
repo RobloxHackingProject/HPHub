9f19cb59f4419f370300dcc2d6089f8d4ede3940
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2023-03-19 17:08:14 +0100

    Update friends.lua

--- friends.lua
+++ friends.lua
@@ -2,11 +2,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
 local Flags = Library.Flags
 --Library.DefaultColor = Color3.fromRGB(65, 143, 232)
-Library:Notification({
-           Text = "Re-toggle your esp after the round is over.",
-           Duration = 5
-       })
-
 local Window = Library:Window({
    Text = "Exploits"
 })
@@ -48,42 +43,53 @@ end
 end
    end
 })
-Window:Button({
+Window:Toggle({
 Text = "Auto-Collect Items",
-Callback = function()
-Library:Notification({
-           Text = "If you get stuck teleport back to the theater.",
-           Duration = 3
+Callback = function(bool)
+if bool then
+       Library:Notification({
+           Text = "It can take some time for the module to be disabled.",
+           Duration = 5
        })
-for i = 1, 24 do
-for __,v in pairs(game:GetService("Workspace"):GetChildren()) do
+local runService = game:GetService("RunService")
+event = runService.RenderStepped:Connect(function()
+for __,v in ipairs(game:GetService("Workspace"):GetChildren()) do
 if v:FindFirstChild("TouchTrigger") then
 local pos = v.TouchTrigger.CFrame
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
 wait(0.1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(374.44, 45, 137.24)
-wait(0.1)
+wait(0.2)
 end
 end
+end)
 end
-end})
+if not bool then
+event:Disconnect()
+end
+end
+})
 
 Window2:Toggle({
    Text = "Friends",
    Callback = function(bool)
-if bool == true then do
+if bool then
+local runService = game:GetService("RunService")
+event = runService.RenderStepped:Connect(function()
 for __,v in pairs(game:GetService("Workspace").Monsters:GetChildren()) do
+ if not v:FindFirstChild("Lol") then
  local esp = Instance.new("Highlight", v)
                 esp.Name = "Lol"
                 esp.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-                esp.FillColor = Color3.new(255, 0, 0)
+                esp.FillColor = Color3.new(0, 0, 255)
 end
 end
+end)
 end
-if bool == false then do
+if not bool then
+event:Disconnect()
 for __,v in pairs(game:GetService("Workspace").Monsters:GetChildren()) do
 v:FindFirstChild("Lol"):Destroy()
-end
 end
 end
 end
@@ -91,22 +97,26 @@ end
 Window2:Toggle({
    Text = "Items",
    Callback = function(bool)
-   if bool == false then do
+if bool then
+local runService = game:GetService("RunService")
+event = runService.RenderStepped:Connect(function()
 for __,v in pairs(game:GetService("Workspace"):GetChildren()) do
 if v:FindFirstChild("TouchTrigger") then
-v:FindFirstChild("Lol"):Destroy()
-end
-end
-end
-end
-if bool == true then do
-for __,v in pairs(game:GetService("Workspace"):GetChildren()) do
-if v:FindFirstChild("TouchTrigger") then
+ if not v:FindFirstChild("Lol") then
  local esp = Instance.new("Highlight", v)
                 esp.Name = "Lol"
                 esp.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
                 esp.FillColor = Color3.new(0, 255, 0)
 end
+end
+end
+end)
+end
+if not bool then
+event:Disconnect()
+for __,v in pairs(game:GetService("Workspace"):GetChildren()) do
+if v:FindFirstChild("TouchTrigger") then
+v:FindFirstChild("Lol"):Destroy()
 end
 end
 end
@@ -129,6 +139,12 @@ Window3:Button({
    Text = "Spawn",
    Callback = function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(175, 33, 272)
+end
+})
+Window3:Button({
+   Text = "Waiting Room",
+   Callback = function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(22.5251369, 7.99802494, 331.021362, -0.999754488, 2.02947303e-09, -0.0221568532, 2.45204634e-09, 1, -1.9044732e-08, 0.0221568532, -1.90943865e-08, -0.999754488)
 end
 })
 
